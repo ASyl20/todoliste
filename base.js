4961a06aee5a3af29ccb89d2fc3b9d2c95f03f14
Sylvain <ksoulkaiser@gmail.com>
2017-07-24 15:56:15 +0200

    first commit

---
+++ base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCy54CPPgLVwEQsY26Y1iPY73LgoEwgn5U",
+    authDomain: "todolist-fb58d.firebaseapp.com",
+    databaseURL: "https://todolist-fb58d.firebaseio.com"
+})
+
+
+export default base;
